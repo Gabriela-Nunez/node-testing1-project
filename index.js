921765a1d38b28f423966affd6adc628f9de06fa
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-04-25 12:39:20 -0500

    exercise 6 is finished along with functions in index.js

--- index.js
+++ index.js
@@ -124,6 +124,9 @@ class Car {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
     // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.name = name
+    this.mpg = mpg
   }
 
   /**
@@ -141,6 +144,15 @@ class Car {
    */
   drive(distance) {
     // ✨ implement
+  const milesCanDrive = this.tank * this.mpg
+  if (distance <= milesCanDrive) {
+    this.odometer = this.odometer + distance
+    this.tank = this.tank - (distance / this.mpg)
+  } else {
+    this.tank = 0
+    this.odometer = this.odometer + milesCanDrive
+  }
+  return this.odometer
   }
 
   /**
@@ -156,6 +168,12 @@ class Car {
    */
   refuel(gallons) {
     // ✨ implement
+    if (gallons <= this.tankSize - this.tank) {
+      this.tank = this.tank + gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
